0af509d13848bb11dca454086a501aad6d74f765
Kevin Wood <kevin.wood75@gmail.com>
2021-11-12 11:06:40 -0500

    adding jenkinsfile

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
+terraform {
+  backend "pg" {
+      conn_str="postgres://tf_user:jandrew28@192.168.2.213/terraform_backend?sslmode=disable"
+  }  
+}
+
 provider "azurerm" {
-  version = "2.46.0"
+  features {}
 }
 
 # Create a resource group
